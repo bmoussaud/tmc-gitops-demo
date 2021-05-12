f5d71ed711ed5cf90d11db48c017c16848300820
Benoit Moussaud <benoit@moussaud.org>
2021-05-12 17:16:20 +0200

    cleanup

---
+++ clean.sh
@@ -0,0 +1,7 @@
+tmc workspace delete bmoussaud-micropet-dev-staging 
+tmc workspace delete bmoussaud-micropet-production
+tmc cluster delete bmoussaud-dev-one
+
+tmc clustergroup delete bmoussaud-dev
+tmc clustergroup delete bmoussaud-prod
+tmc clustergroup delete bmoussaud-stage
