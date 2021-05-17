0d059e636049615e4af34afd044013f9dd7616f4
Benoit Moussaud <benoit@moussaud.org>
2021-05-17 10:17:13 +0200

    GH

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,14 @@ time=$(date)
 
 #echo "TMC Login"
 #tmc login --no-configure --name gitops
+cd /github/workspace
 
 echo "============ IN  /github/workspace"
-find /github/workspace
+find . -ls
 echo "============ OUT /github/workspace"
 
-cd /github/workspace
 echo "============ Apply.sh"
 /usr/src/app/apply.sh 
+echo "============ /Apply.sh"
 
 echo "::set-output name=time::$time"
