6556c60b4329a52c2119e5c37cc881ae1b1668e9
Benoit Moussaud <benoit@moussaud.org>
2021-05-17 12:11:57 +0200

    manage TMC token api

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Hello Benoit $1"
 time=$(date)
 
 export TMC_API_TOKEN=$2
-echo "TMC Login with TMC_API_TOKEN ${TMC_API_TOKEN}"
+echo "TMC Login with TMC_API_TOKEN '${TMC_API_TOKEN}'"
 tmc login --no-configure --name gitops
 
 echo "Move to /github/workspace"
